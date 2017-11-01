ada43cbaa75b9b6d2608ca07df54a5feb577653a
ttran9 <toddtran9@gmail.com>
2017-11-01 11:57:11 -0700

    Video 7. Updating this repo to reflect the starting state.

--- src/test/java/guru/springframework/SpringmvcApplicationTests.java
+++ src/test/java/guru/springframework/SpringmvcApplicationTests.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringmvcApplication.class)
 public class SpringmvcApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
