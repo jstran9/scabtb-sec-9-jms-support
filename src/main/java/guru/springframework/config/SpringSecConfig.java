1d3f35be48a1a24a52d3881a138fd376449fb388
ttran9 <toddtran9@gmail.com>
2017-11-02 20:28:06 -0700

    Video 28. Wiring up a dao authentication provider.

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -2,15 +2,27 @@ package guru.springframework.config;
 
 import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
 import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationProvider authenicationProvider;
+
+    @Autowired // autowire this back into this config class.
+    @Qualifier("daoAuthenticationProvider")
+    public void setAuthenicationProvider(AuthenticationProvider authenicationProvider) {
+        this.authenicationProvider = authenicationProvider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
@@ -24,6 +36,7 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
                                                                UserDetailsService userDetailsService) {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        // goes to our database, get a user object and convert it over to the UserDetails object.
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         return daoAuthenticationProvider;
     }
@@ -31,5 +44,13 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+    @Autowired
+    public void configureAuthManager(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        // how spring security sets up the authentication providers.
+        // spring context will inject in the provider and then the builder will
+        // then use our authenticationProvider object. (IoC here since Spring is handling this for us).
+        authenticationManagerBuilder.authenticationProvider(authenicationProvider);
     }
 }
